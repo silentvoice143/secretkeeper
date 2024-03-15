ee8096a98d31edf4ee47fbb502271521f006a514
Satyam Kumar <silentvoice4143@gmail.com>
2024-03-15 18:36:09 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 const App: React.FC = () => {
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-red-200 App">
-      <div className="w-[400px]  bg-[#FFBBBB] flex flex-col items-center relative">
+      <div className="w-[400px] max-[700px]:w-full max-[700px]:h-full py-8  bg-[#FFBBBB] flex flex-col items-center relative">
         <h1 className="text-xl">SecretKeeper</h1>
         <QRCodeGenerator />
       </div>
